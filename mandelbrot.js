13a03f1a8c7e410ceea68b1bbf802923bc7537d0
Tomi Kalmi <tomi.kalmi@gmail.com>
2024-02-23 03:29:31 +0200

    Precompute each n

--- mandelbrot.js
+++ mandelbrot.js
@@ -21,12 +21,26 @@ function getMandelbrotIterations(cRe, cIm, maxIterations) {
   return n;
 }
 
-function drawMandelbrot(maxIterations) {
+function getCachedNs(maxIterations) {
+  const cachedNs = new Array(width * height);
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       const cRe = RE_AXIS_START + (x / width) * (RE_AXIS_END - RE_AXIS_START);
       const cIm = IM_AXIS_START + (y / height) * (IM_AXIS_END - IM_AXIS_START);
-      const n = getMandelbrotIterations(cRe, cIm, maxIterations);
+      cachedNs[x + y * width] = getMandelbrotIterations(
+        cRe,
+        cIm,
+        maxIterations
+      );
+    }
+  }
+  return cachedNs;
+}
+
+function drawMandelbrot(maxIterations) {
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      const n = Math.min(maxIterations, cachedNs[x + y * width]);
       const color = 255 * (1 - n / maxIterations);
       ctx.fillStyle = `rgb(${color}, ${color}, ${color})`;
       ctx.fillRect(x, y, 1, 1);
@@ -34,8 +48,11 @@ function drawMandelbrot(maxIterations) {
   }
 }
 
+const cachedNs = getCachedNs(10_000);
+
 let maxIterations = 1;
 function step() {
+  console.log('N max:', maxIterations);
   drawMandelbrot(maxIterations);
   maxIterations++;
   requestAnimationFrame(step);
